d0f9eb0d2797a402bbdec163f1f52b68294f6bb1
Vivi Song <vivisong1903@gmail.com>
2019-09-16 20:39:12 -0500

    Fox Mesh and Run Anim
    Fox has a mesh collider and she is standing on the ground. Idle and run animation transition is added when arrow keys are pressed.

--- Computer Graphics Project/Assets/C# Scripts/MovementControl.cs
+++ Computer Graphics Project/Assets/C# Scripts/MovementControl.cs
@@ -8,9 +8,10 @@ public class MovementControl : MonoBehaviour
 	// Create our variables here.
 	private float speed = 10;
 	private Rigidbody body = null;
-
-	// Use this for initialization
-	void Start() 
+    public bool isRun = false;
+    public Animator animator;
+    // Use this for initialization
+    void Start() 
 	{
 		// Get the RigidBody values from the RigidBody component attached to 
 		// the GameObject that has this script.
@@ -31,6 +32,16 @@ public class MovementControl : MonoBehaviour
 		// Create variables exclusive to this function here.
 		float horizMove = Input.GetAxis("Horizontal");
 		float vertiMove = Input.GetAxis("Vertical");
+        if(Input.GetKey("up")|| Input.GetKey("down")||Input.GetKey("left")|| Input.GetKey("right"))
+        {
+            isRun = true;
+            animator.SetBool("isRun", true);
+        }
+        else
+        {
+            isRun = false;
+            animator.SetBool("isRun", false);
+        }
 		Vector3 moveVector = new Vector3(horizMove, 0.0f, vertiMove);	// We're using the horizontal and vertical axes to create a movement vector for us.
 
 		// Apply force to the GameObject through RigidBody to create movement.
