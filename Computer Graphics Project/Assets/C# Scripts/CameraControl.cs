2efbc9282688000c0931919e115349af244fef94
NocturneLight <55106060+NocturneLight@users.noreply.github.com>
2019-09-17 20:59:01 -0500

    Fixes to mesh, added additional camera movements.

--- Computer Graphics Project/Assets/C# Scripts/CameraControl.cs
+++ Computer Graphics Project/Assets/C# Scripts/CameraControl.cs
@@ -8,6 +8,7 @@ public class CameraControl : MonoBehaviour
 	// Create our variables here.
 	public GameObject playerObject; // Allows us to attach a GameObject to the script in the Unity Editor.
 	private Vector3 offset;
+	private float cameraSpeed = 10.0f;
 
 	// Use this for initialization
 	void Start() 
@@ -19,7 +20,18 @@ public class CameraControl : MonoBehaviour
 	// Update is called once per frame
 	void Update() 
 	{
-		// Offset the position to prevent the camera from rotating with the GameObject.
-		this.transform.position = playerObject.transform.position + offset;
+		// While right-click is being held...
+		if (Input.GetMouseButton(1))
+		{
+			// Calculate a value for rotation and store it in the offset variable.
+			offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * cameraSpeed, Vector3.up) * offset;
+
+			// Offset the position to prevent the camera from rotating with the GameObject
+			// along undesirable axes.
+			this.transform.position = playerObject.transform.position + offset;
+		}
+
+		// Rotate the transform such that the forward vector points at worldPosition.
+		this.transform.LookAt(playerObject.transform.position);
 	}
 }
--- Computer Graphics Project/Assets/C# Scripts/MovementControl.cs
+++ Computer Graphics Project/Assets/C# Scripts/MovementControl.cs
@@ -7,7 +7,6 @@ public class MovementControl : MonoBehaviour
 
 	// Create our variables here.
 	private float speed = 10;
-	private bool isRun = false;
 	private Rigidbody body = null;
     public Animator animator = null;
     
@@ -40,16 +39,14 @@ public class MovementControl : MonoBehaviour
 		Vector3 moveVector = new Vector3(horizMove, 0.0f, vertiMove);	// We're using the horizontal and vertical axes to create a movement vector for us.
 
 		// If pressing a key associated with movement, apply the walking animation.
-        if(Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right") ||
-		   Input.GetKey("w")  || Input.GetKey("s")    || Input.GetKey("a")    || Input.GetKey("d"))
+        if (Input.GetKey("up") || Input.GetKey("down") || Input.GetKey("left") || Input.GetKey("right") ||
+		    Input.GetKey("w")  || Input.GetKey("s")    || Input.GetKey("a")    || Input.GetKey("d"))
         {
-            isRun = true;
             animator.SetBool("isRun", true);
         }
 		// Otherwise, disable the walking animation.
         else
         {
-            isRun = false;
             animator.SetBool("isRun", false);
         }
 
