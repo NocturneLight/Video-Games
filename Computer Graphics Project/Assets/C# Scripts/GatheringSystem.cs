31a759e65795c8a112b07835bb3900b2e9d5bfb6
NocturneLight <55106060+NocturneLight@users.noreply.github.com>
2019-09-21 16:56:32 -0500

    Implemented Preliminary Gathering System.

--- Computer Graphics Project/Assets/C# Scripts/GatheringSystem.cs
+++ Computer Graphics Project/Assets/C# Scripts/GatheringSystem.cs
@@ -1,16 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class GatheringSystem : MonoBehaviour {
+public class GatheringSystem : MonoBehaviour 
+{
+	// Create variables here.
+	public Text itemCountText = null;
+	private int itemCount = 0;
+
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start () 
+	{
+		// Default text for the item count.
+		itemCountText.text = "0/?";
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () 
+	{
 		
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		// If the object collided with is a machine piece...
+		if (other.gameObject.CompareTag("Machine Piece"))
+		{
+			// Make the item disappear.
+			other.gameObject.SetActive(false);
+
+			// Then update the item count.
+			itemCountText.text = ++itemCount + "/?";
+		}
 	}
 }
---
+++ Computer Graphics Project/Assets/C# Scripts/ItemCheck.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Needed to work with UI objects.
+
+public class ItemCheck : MonoBehaviour
+{
+    // Create variables exclusive to this function here.
+    private Text textObject = null;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Get the component so we can work with it.
+        textObject = this.GetComponent<Text>();
+
+        // Default to the GUI being invisible.
+        textObject.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // Called for the rendering and handling of GUI events.
+    void OnGUI()
+    {
+        // If the Q key was pressed, show the GUI.
+        textObject.enabled = Input.GetButton("Open Inventory") ? true : false;
+    }
+}
