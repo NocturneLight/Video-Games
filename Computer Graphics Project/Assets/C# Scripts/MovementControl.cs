9e60368c1a3769247cd589cd403ba089cce20b0f
NocturneLight <55106060+NocturneLight@users.noreply.github.com>
2019-10-15 18:37:46 -0500

    Recreated Original Build.
    Recreated my original build from before I messed up the merge.

--- Computer Graphics Project/Assets/C# Scripts/MovementControl.cs
+++ Computer Graphics Project/Assets/C# Scripts/MovementControl.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 
 public class MovementControl : MonoBehaviour 
 {
-
 	// Create our variables here.
-	public float speed = 10;
+    private const float jumpMultip = 4.5f;
+    private int jumpCount = 0;
+	public int jumpLimit = 2;
+	public float speed = 5.0f;
+	bool ground = false;
 	private Rigidbody body = null;
     public Animator animator = null;
     
@@ -20,29 +23,105 @@ public class MovementControl : MonoBehaviour
 		// Get the Animator values from the Animator component attached to 
 		// the GameObject that has this script.
 		animator = this.GetComponent<Animator>();
-
 	}
 	
 	// Update is called once per frame
 	void Update() 
 	{
-		
+
 	}
 
 	// FixedUpdate() is called just before performing any Physics calculations.
     // This is where Physics code will go.
 	void FixedUpdate()
 	{
-		// Create variables exclusive to this function here.
-		float horizMove = Input.GetAxis("Horizontal");
-		float vertiMove = Input.GetAxis("Vertical");
-		Vector3 moveVector = new Vector3(horizMove, 0.0f, vertiMove);	// We're using the horizontal and vertical axes to create a movement vector for us.
+		// Create variables here.
+		Transform cameraTransform = Camera.main.transform;
+		Vector3 moveVector = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
+		Vector3 forwardCam = cameraTransform.forward.normalized;
+		Vector3 rightCam = cameraTransform.right.normalized;
 
-		
-		// If pressing a key associated with movement, apply the walking animation.
+		// Set the y value of forwardCam and rightCam to 0 to prevent side effects.
+		forwardCam.y = 0.0f;
+		rightCam.y = 0.0f;
+
+		// If a movement button was pressed, set the corresponding animation in accordance
+		// with the isRun variable.
 		animator.SetBool("isRun", (Input.GetButton("Horizontal") || Input.GetButton("Vertical")) ? true : false);
 
-		// Assign our object's velocity a speed vector using speed multiplied with the Horizontal and Vertical axes.
-		body.velocity = new Vector3(horizMove * speed, 0.0f, vertiMove * speed);
+		// See if the requirements for jumping have been met.
+		DoJump();
+
+		// Apply the necessary rotation.
+		doRotate();
+
+		// Apply the movement to our GameObject.
+		body.transform.position += (forwardCam * moveVector.z + rightCam * moveVector.x) * speed * Time.deltaTime;
+	}
+
+	// A function called upon colliding with an object.
+	void OnCollisionStay(Collision collision)
+	{
+		// If the object we collided with is an object with the tag "Ground"...
+		if (collision.gameObject.tag == "Ground")
+		{
+			// Set ground to true.
+			ground = true;
+		}
+	}
+
+	// Create our other functions here.
+	void DoJump()
+	{
+		// If the user presses the jump button...
+		if (Input.GetButtonDown("Jump") && ground && jumpCount < jumpLimit)
+		{
+			// Apply the jump as an impulse force.
+			body.AddForce(Vector3.up * body.mass * jumpMultip, ForceMode.Impulse);
+
+			// Increment jumpCount by 1 jump because we jumped.
+			jumpCount++;
+
+			// Set ground to false because we are not grounded when pressing jump.
+			ground = false;
+		}
+
+		// If the model hits the jump limit...
+		if (jumpCount >= jumpLimit)
+		{
+			// Reset the jump count.
+			jumpCount = 0;
+		}
+	}
+
+	void doRotate()
+	{
+		// Create variables here.
+		Quaternion camRote = Camera.main.transform.parent.rotation;
+
+		// Set values x and z of camRote to 0 so we rotate along the y axis.
+		camRote.x = 0.0f;
+		camRote.z = 0.0f;
+
+		// Apply the rotation.
+		//body.transform.rotation = camRote;
+
+		if (Input.GetKey("w") || Input.GetKey("up"))
+		{
+			// Transition between rotation values and end up facing away from the camera.
+			body.transform.rotation = Quaternion.RotateTowards(body.transform.rotation, camRote, 8.0f);
+			
+		}
+		else if (Input.GetKey("s") || Input.GetKey("down"))
+		{
+			// Get the forward vector of the camera's parent.
+			Vector3 temp = -Camera.main.transform.parent.forward;
+
+			// Set y to 0 because we don't want the GameObject to rotate around y.
+			temp.y = 0.0f;
+
+			// Transition the rotation and face towards the camera.
+			body.transform.rotation = Quaternion.RotateTowards(body.transform.rotation, Quaternion.LookRotation(temp, Camera.main.transform.up), 8.0f);
+		}	
 	}
 }
